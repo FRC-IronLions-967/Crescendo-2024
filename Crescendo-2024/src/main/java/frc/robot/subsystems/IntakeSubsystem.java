fe8f27ace2f4fccc1eb73e1845072809731490bd
nastark123 <vikingsiowa28@gmail.com>
2024-01-18 20:33:20 -0600

    Added methods to IntakeSubsystem

--- Crescendo-2024/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ Crescendo-2024/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -8,19 +8,22 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Utils.Constants;
 
 
 public class IntakeSubsystem extends SubsystemBase {
 
   private CANSparkMax intakeMotor;
   private SparkPIDController intakeMotorPID;
-  private CANSparkMax intakePivotMotor;
-  private SparkPIDController intakePivotMotorPID;
+  private CANSparkMax pivotMotor;
+  private SparkPIDController pivotMotorPID;
   private DigitalInput isNoteIn;
-  private double speed;
+  private boolean hasNote;  
 
   public boolean intakePostionOut;
   /** Creates a new IntakeSubsystem. */
@@ -32,29 +35,44 @@ public class IntakeSubsystem extends SubsystemBase {
     intakeMotorPID.setD(0.0);
     intakeMotorPID.setFF(0.0);
 
-    intakePivotMotor = new CANSparkMax(10, MotorType.kBrushless);
-    intakePivotMotorPID = intakePivotMotor.getPIDController();
-    intakePivotMotorPID.setP(1.0);
-    intakePivotMotorPID.setI(0.0);
-    intakePivotMotorPID.setD(0.0);
-    intakePivotMotorPID.setFF(0.0);
+    pivotMotor = new CANSparkMax(10, MotorType.kBrushless);
+    pivotMotorPID = pivotMotor.getPIDController();
+    pivotMotor.getAbsoluteEncoder(Type.kDutyCycle).setPositionConversionFactor(360.0);
+    pivotMotorPID.setFeedbackDevice(pivotMotor.getAbsoluteEncoder(Type.kDutyCycle));
+    pivotMotorPID.setP(1.0);
+    pivotMotorPID.setI(0.0);
+    pivotMotorPID.setD(0.0);
+    pivotMotorPID.setFF(0.0);
 
     isNoteIn = new DigitalInput(2);
+    hasNote = false;
   }
   
   public void runIntake(double speed) {
+    if(speed < -Constants.kMaxNEOSpeed) speed = -Constants.kMaxNEOSpeed;
+    if(speed > Constants.kMaxNEOSpeed) speed = Constants.kMaxNEOSpeed;
+
     intakeMotorPID.setReference(speed, ControlType.kVelocity);
   }
 
-  public void moveIntake(double speed) {
+  public void moveIntake(double position) {
+    if(position < Constants.kIntakeMinPosition) position = Constants.kIntakeMinPosition;
+    if(position > Constants.kIntakeMaxPosition) position = Constants.kIntakeMaxPosition;
+  
+    pivotMotorPID.setReference(position, ControlType.kPosition);
+  }
 
+  public double getIntakePosition() {
+    return pivotMotor.getAbsoluteEncoder(Type.kDutyCycle).getPosition();
+  }
+
+  public boolean isNoteIn() {
+    return hasNote;
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    if (isNoteIn.get() && speed > 0) {
-      intakeMotorPID.setReference(0, ControlType.kVelocity);
-    }
+    hasNote = isNoteIn.get();
   }
 }
